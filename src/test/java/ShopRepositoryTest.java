26a5560beedb1913fcd88f6bd047f5f93f9d6c93
Kifo <i.g.a.n.garsia@gmail.com>
2025-03-11 21:28:18 +0300

    Initial commit

---
+++ src/test/java/ShopRepositoryTest.java
@@ -0,0 +1,33 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ShopRepositoryTest {
+
+    @Test
+    void shouldRemoveExistingProduct() {
+        ShopRepository repository = new ShopRepository();
+        Product product1 = new Product(1, "Хлеб", 50);
+        Product product2 = new Product(2, "Молоко", 100);
+
+        repository.add(product1);
+        repository.add(product2);
+
+        repository.removeById(1);
+
+        Product[] expected = { product2 };
+        assertArrayEquals(expected, repository.findAll());
+    }
+        @Test
+        void shouldThrowNotFoundExceptionWhenRemovingNonExistingProduct() {
+            ShopRepository repository = new ShopRepository();
+            Product product1 = new Product(1, "Хлеб", 50);
+            Product product2 = new Product(2, "Молоко", 100);
+
+            repository.add(product1);
+            repository.add(product2);
+
+            assertThrows(NotFoundException.class, () -> {
+                repository.removeById(3);
+            });
+        }
+    }
